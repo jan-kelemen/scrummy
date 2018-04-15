fb5a1658434c7380ce1703a42c37ab913c06dbfe
Jan Kelemen <jkeleme@gmail.com>
2018-04-15 15:23:58 +0200

    add runtime initialization

---
+++ src/Scrummy/Runtime.Common/Initialization/RuntimeInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using Scrummy.Domain.Repositories.Factories;
+using Scrummy.Domain.UseCases.Initialization;
+using Scrummy.Domain.UseCases.Interfaces;
+using Scrummy.Persistence.Infrastructure;
+using Scrummy.Persistence.Infrastructure.Initialization;
+
+namespace Runtime.Common.Initialization
+{
+    public static class RuntimeInitializer
+    {
+        public static IRepositoryFactory RepositoryFactory { get; private set; }
+
+        public static IUseCaseFactoryProvider UseCaseFactoryProvider { get; private set; }
+
+        public static void Initialize()
+        {
+            InitializePersistence();
+            InitializeUseCases();
+        }
+
+        private static void InitializePersistence()
+        {
+            RepositoryFactory = PersistenceInitializer.Initialize(SupportedPersistenceType.MongoDB);
+        }
+
+        private static void InitializeUseCases()
+        {
+            UseCaseFactoryProvider = UseCaseInitializer.Initialize(RepositoryFactory);
+        }
+    }
+}
